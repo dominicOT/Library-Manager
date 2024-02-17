f858ba483518542e181a10c1b2a323f5e84dbe30
Dominic Oladapo-Tonade <dominicoladapotonade@gmail.com>
2024-02-17 14:31:02 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import csv
+import os
+
+window = tk.Tk()
+window.title("UniMak Library Management System")
+window.geometry("700x500")
+
+titleL = tk.Label(window, text="UniMak Library Management", font=("Monterrat", 20.7))
+titleL.grid(row=0, column=1, padx=10, pady=10)
